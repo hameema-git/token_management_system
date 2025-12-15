a0fed260892f3d553e648d5c458fb5b775708f38
Hameema <hameema@gmail.com>
2025-12-15 08:13:35 +0530

    new commit9

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ShopProvider } from "./context/ShopContext";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
